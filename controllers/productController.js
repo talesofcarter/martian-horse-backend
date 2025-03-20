78fe54489e583ba8be013b4ea21af574623f0daa
codebycarter <kelvinjuma99@gmail.com>
2025-03-20 15:37:01 +0300

    logic for remove product

--- controllers/productController.js
+++ controllers/productController.js
@@ -67,7 +67,15 @@ const listProducts = async (req, res) => {
 };
 
 // function for removing product
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+  try {
+    await productModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Product successfully removed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // function for single product info.
 const singleProduct = async (req, res) => {};
