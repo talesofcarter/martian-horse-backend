ebc7259e8e7e3ea1041e061e02b617d257c86e18
codebycarter <kelvinjuma99@gmail.com>
2025-03-20 16:07:13 +0300

    user authentication API

--- controllers/userController.js
+++ controllers/userController.js
@@ -61,7 +61,7 @@ const registerUser = async (req, res) => {
       });
     }
 
-    // hasing user password
+    // hashing user password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -83,6 +83,23 @@ const registerUser = async (req, res) => {
 };
 
 // route for admin login
-const adminLogin = async (req, res) => {};
+const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export { loginUser, registerUser, adminLogin };
