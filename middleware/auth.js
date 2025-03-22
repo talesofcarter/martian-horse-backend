ff105d9309046b068e98c7feb6b49bf8d81febb8
codebycarter <kelvinjuma99@gmail.com>
2025-03-22 17:56:44 +0300

    fixed the issues causing the requests to not reach db

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const authUser = async (req, res) => {
+const authUser = async (req, res, next) => {
   const { token } = req.headers;
 
   if (!token) {
@@ -8,7 +8,7 @@ const authUser = async (req, res) => {
   }
 
   try {
-    const token_decode = jwt.verify(token, process.even.JWT_SECRET);
+    const token_decode = jwt.verify(token, process.env.JWT_SECRET);
     req.body.userId = token_decode.id;
     next();
   } catch (error) {
